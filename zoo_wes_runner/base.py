977691a90a56a58f68fe692c46111ada6df4a082
Alex Manning <alexander.manning@stfc.ac.uk>
2023-11-21 14:28:53 +0000

    Fix import problems.

--- zoo_wes_runner/base.py
+++ zoo_wes_runner/base.py
@@ -5,25 +5,20 @@ These are derived here from the zoo-calrissian-runner because no generic abstrac
 import logging
 import types
 
+import zoo
 import zoo_calrissian_runner
 
 logger = logging.getLogger()
-
-# todo: where does zoo come from?
-zoo = zoo_calrissian_runner.zoo
 
 
 class BaseZooRunner(zoo_calrissian_runner.ZooCalrissianRunner):
     """Mangle the ZooCalrissianRunner to be a base class to inherit from."""
 
-    def __new__(cls):
-        """Create a ZooRunner without kubernetes-specific things."""
-        cls.shorten_namespace = None
-        cls.get_namespace_name = None
-        return cls
+    shorten_namespace = None
+    get_namespace_name = None
 
     def prepare(self):
-        """Generic pre-execution steps which might apply to all handlers."""
+        """Generic pre-execution which applies to all handlers."""
 
         logger.info("execution started")
         self.update_status(progress=2, message="starting execution")
